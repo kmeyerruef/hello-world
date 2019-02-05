c7918c42b1bac49513d367321bebb15faf96de92
Kris Meyer-Ruef <kris.meyer-ruef@pcc.edu>
2019-02-05 14:36:11 -0800

    added notes for author

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package edu.pcc.cis.helloworld;
 
 import java.util.Date;
-
+// Author: Kris Meyer-Ruef
+// Project: Hello World
+// 2/5/2018
 public class HelloWorld {
 
     public static void main (String[] args)
