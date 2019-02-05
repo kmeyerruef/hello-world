3827eddc7f7ef2d7fd6884bb281d26e9596ae173
Kris Meyer-Ruef <kris.meyer-ruef@pcc.edu>
2019-02-05 14:28:08 -0800

    actually added a print statement for HelloWorld

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
 
-    private void helloWorld()
+    public static void main (String[] args)
     {
         System.out.println("Hello World!");
     }
